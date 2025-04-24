61cf3c2081188268c37702ae14cab0efd4e20c2e
Hunter Pope <hunter.pope23@gmail.com>
2025-04-23 21:08:27 -0500

    Started validation functions

--- version-1/app.h
+++ version-1/app.h
@@ -4,5 +4,11 @@
 // function prototypes
 void flush_buffer();
 int start_menu();
+void validate_axiom(char *axiom);
+void rules_for(char *axiom, int indices[]);
+void validate_rules(char *rules);
+int validate_iterations();
+float validate_turn_angle();
+float validate_start_direction();
 
 #endif
